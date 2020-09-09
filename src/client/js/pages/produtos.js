50907fedc36559eab7a3691946d37e09ca2c2397
alexandre-queiroz <alexandre.queiroz@aluno.faculdadeimpacta.com.br>
2020-09-09 15:33:19 -0300

    alterar e inativar

--- src/client/js/pages/produtos.js
+++ src/client/js/pages/produtos.js
@@ -1,11 +1,21 @@
+const btnBuscar = $('#btn-buscar');
+const btnCadastrar = $('#btn-cadastrar');
+const btnCadastrarNovoProduto = $('#cadastrar-produto')
+const btnAlterarProduto = $('#alterar-produto')
+const btnInativarProduto = $('#inativar-produto')
+
+const txtProduto = $('#txt-produto');
+const nomeNovoProduto = $('#nome-novo-produto')
+const descNovoProduto = $('#desc-novo-produto')
+const precoNovoProduto = $('#preco-novo-produto')
+
+const modalProduto = $('#modal-produto');
+const modalProdutoTitle = $('#modal-produto-title')
+
+const modalInativarProduto = $('#modal-inativar-produto')
+const modalInativarProdutoTitle = $('#modal-inativar-produto-title')
+
 $(document).ready(function () {
-
-    btnBuscar = $('#btn-buscar');
-    btnCadastrar = $('#btn-cadastrar');
-
-    txtProduto = $('#txt-produto');
-
-    modalProduto = $('#modal-produto');
 
     // Inicialização da DataTable
     dtProducts = $("#table-products").DataTable({
@@ -46,23 +56,102 @@ $(document).ready(function () {
         }
     })
 
+    // Lista os produtos
     listarProdutos();
 
+    // Faz a busca na tabela
     btnBuscar.click(function () {
 
         dtProducts.search(txtProduto.val()).draw();
 
     })
 
-    btnCadastrar.click(function(){
+    // Invoca o modal de cadastro
+    btnCadastrar.click(function () {
+
+        modalProdutoTitle.text('Cadastrar produto')
+
+        btnCadastrarNovoProduto.removeClass('d-none');
 
         modalProduto.modal('show')
+
+    })
+
+    // Realiza o cadastro
+    btnCadastrarNovoProduto.click(function () {
+
+        const novoProduto = {};
+
+        novoProduto.nome = nomeNovoProduto.val();
+        novoProduto.descricao = descNovoProduto.val();
+        novoProduto.preco = precoNovoProduto.val();
+
+        cadastrarProdutos(novoProduto);
+
+    })
+
+    // Invoca e preenche o modal de alteração
+    dtProducts.on('click', 'tr td a i.fa-pencil', function (e) {
+
+        const row = $(this).closest("tr")
+        const dadosProduto = dtProducts.row(row).data()
+
+        const produto = {};
+
+        produto.id = dadosProduto[6]
+        produto.nome = dadosProduto[0]
+        produto.descricao = dadosProduto[1]
+        produto.preco = dadosProduto[7]
+
+        btnAlterarProduto.removeClass('d-none')
+        modalProdutoTitle.text('Alterar produto: ' + produto.nome)
+        modalProduto.modal('show')
+
+        nomeNovoProduto.val(produto.nome);
+        descNovoProduto.val(produto.descricao);
+        precoNovoProduto.val(produto.preco);
+
+        // Salva a alteração
+        btnAlterarProduto.click(function () {
+
+            produto.nome = nomeNovoProduto.val();
+            produto.descricao = descNovoProduto.val();
+            produto.preco = precoNovoProduto.val();
+
+            alterarProduto(produto);
+
+        })
+
+    })
+
+    // Invoca e preenche o modal de inativação
+    dtProducts.on('click', 'tr td a i.fa-close', function (e) {
+
+        const row = $(this).closest("tr")
+        const dadosProduto = dtProducts.row(row).data()
+
+        const produto = {};
+
+        produto.id = dadosProduto[6]
+        produto.nome = dadosProduto[0]
+        produto.status = dadosProduto[8]
+
+        modalInativarProduto.modal('show');
+        modalInativarProdutoTitle.text('Inativar: ' + produto.nome)
+
+        btnInativarProduto.click(function () {
+
+            inativarProduto(produto);
+
+        })
 
     })
 
 });
 
 function listarProdutos() {
+
+    dtProducts.clear().draw();
 
     $.ajax({
         url: '/produtos', // URL do recurso requisitado
@@ -89,10 +178,14 @@ function listarProdutos() {
                 // Coluna 04
                 status,
                 // Coluna 05
-                '<a href="#"><i class="fa fa-pencil color-muted mr-1"></i></a>' +
-                '<a href="#"><i class="fa fa-close color-danger"></i></a>',
-                //Hidden 06
-                produto.id
+                '<a href="javascript:void(0)"><i class="fa fa-pencil color-muted mr-1"></i></a>' +
+                '<a href="javascript:void(0)"><i class="fa fa-close color-danger"></i></a>',
+                // Hidden 06
+                produto.id,
+                // Hidden 07
+                produto.preco,
+                // Hidden 08
+                produto.status
             ])
         })
 
@@ -101,6 +194,111 @@ function listarProdutos() {
     }).fail(function (error) {
 
         alert('Ocorreu um erro, tente novamente')
+
+    })
+
+}
+
+function cadastrarProdutos(novoProduto) {
+
+    btnCadastrarNovoProduto.prop('disabled', true);
+
+    $.ajax({
+        url: '/produtos', // URL do recurso requisitado
+        method: 'POST', // método de requisição solicitado
+        dataType: 'json', // tipo de resposta esperada do servidor
+        contentType: 'application/json', // tipo de contéudo enviado
+        data: JSON.stringify(novoProduto) // dados a serem enviados no corpo da requisiçãso
+
+    }).done(function (resposta) {
+
+        listarProdutos();
+
+        dtProducts.search(novoProduto.nome).draw();
+
+    }).fail(function (error) {
+
+        alert('Ocorreu um erro, tente novamente')
+
+    }).always(function () {
+
+        btnCadastrarNovoProduto.prop('disabled', false);
+
+        btnCadastrarNovoProduto.addClass('d-none');
+
+        nomeNovoProduto.val("");
+        descNovoProduto.val("");
+        precoNovoProduto.val("");
+
+        modalProduto.modal('hide');
+
+    })
+
+}
+
+function alterarProduto(produto) {
+
+    btnAlterarProduto.prop('disabled', true);
+
+    $.ajax({
+        url: '/produtos/' + produto.id, // URL do recurso requisitado
+        method: 'PUT', // método de requisição solicitado
+        dataType: 'json', // tipo de resposta esperada do servidor
+        contentType: 'application/json', // tipo de contéudo enviado
+        data: JSON.stringify(produto) // dados a serem enviados no corpo da requisiçãso
+
+    }).done(function (resposta) {
+
+        listarProdutos();
+
+        dtProducts.search(produto.nome).draw();
+
+    }).fail(function (error) {
+
+        alert('Ocorreu um erro, tente novamente')
+
+    }).always(function () {
+
+        btnAlterarProduto.prop('disabled', false);
+
+        btnAlterarProduto.addClass('d-none');
+
+        nomeNovoProduto.val("");
+        descNovoProduto.val("");
+        precoNovoProduto.val("");
+
+        modalProduto.modal('hide');
+
+    })
+
+}
+
+function inativarProduto(produto) {
+
+    btnInativarProduto.prop('disabled', true);
+
+    $.ajax({
+        url: '/produtos/' + produto.id, // URL do recurso requisitado
+        method: 'DELETE', // método de requisição solicitado
+        dataType: 'json', // tipo de resposta esperada do servidor
+        contentType: 'application/json', // tipo de contéudo enviado
+        data: JSON.stringify(produto) // dados a serem enviados no corpo da requisiçãso
+
+    }).done(function (resposta) {
+
+        listarProdutos();
+
+        dtProducts.search(produto.nome).draw();
+
+    }).fail(function (error) {
+
+        alert('Ocorreu um erro, tente novamente')
+
+    }).always(function () {
+
+        btnInativarProduto.prop('disabled', false);
+
+        modalInativarProduto.modal('hide');
 
     })
 
